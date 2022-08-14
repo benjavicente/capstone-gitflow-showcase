1d998d33a51c54d9628a1cb6b7285da624d978ed
Benjamín Vicente <benjavicente@uc.cl>
2022-08-14 17:03:07 -0400

    add top days to cli

--- twitter_capstone_cly/func/__init__.py
+++ twitter_capstone_cly/func/__init__.py
@@ -1,3 +1,4 @@
+from .top_days import top_days
 from .top_hashtags import top_hashtags
 from .top_retweeted import top_retweeted
 from .top_users import top_users_by_tweets
--- twitter_capstone_cly/func/top_days.py
+++ twitter_capstone_cly/func/top_days.py
@@ -1,1 +1,16 @@
 
+import sqlite3
+from pathlib import Path
+
+
+def top_days(db_path: Path = Path("data.db")):
+    with sqlite3.connect(db_path) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT strftime('%Y-%m-%d', date) AS day, COUNT(*) AS tweets
+            FROM tweets
+            GROUP BY day
+            ORDER BY tweets DESC
+            LIMIT 10
+        """)
+        return cursor.fetchall()
